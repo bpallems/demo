42c1409086edb744f5c95156da7ec302da42f263
Bhanu Pallemsetty <bhanu_pallemsetty@persistent.com>
2022-02-09 18:58:47 +0530

    updated files

--- tag_push.bat
+++ tag_push.bat
@@ -2,5 +2,5 @@ COPY %WORKSPACE%\webapp\target\webapp.war %WORKSPACE%
 docker stop bhanudocker3315/regapp
 docker rm bhanudocker3315/regapp
 echo tag - %tag%
-docker build -t bhanudocker3315/regapp:latest -t bhanudocker3315/regapp:%tag% -f ./worker/Dockerfile %WORKSPACE%
+docker build -t bhanudocker3315/regapp:latest -t bhanudocker3315/regapp:%tag% %WORKSPACE%
 docker run -d --name simple-devops-container -p 8090:8080 simple-devops-image
