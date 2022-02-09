884cec6927396fc28428700515a54565fb4affa8
Bhanu Pallemsetty <bhanu_pallemsetty@persistent.com>
2022-02-09 21:40:11 +0530

    updated file

--- tag_push.bat
+++ tag_push.bat
@@ -3,7 +3,7 @@ docker stop simple-regapp
 docker rm simple-regapp
 echo tag - %tag%
 docker build -t bhanudocker3315/regapp:latest -t bhanudocker3315/regapp:%tag% %WORKSPACE%
-docker login
+echo "ab2534c7-32da-4eb7-92ff-f2ea5ab5a89c" | docker login -u "bhanudocker3315" --password-stdin
 docker push bhanudocker3315/regapp:latest
 docker push bhanudocker3315/regapp:%tag%
 docker run -d --name simple-regapp -p 8090:8080 bhanudocker3315/regapp
