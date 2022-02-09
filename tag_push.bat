733351a9ee293437303efcc638bfed27b3960373
Bhanu Pallemsetty <bhanu_pallemsetty@persistent.com>
2022-02-09 19:06:56 +0530

    updated file

--- tag_push.bat
+++ tag_push.bat
@@ -3,4 +3,4 @@ docker stop simple-regapp
 docker rm simple-regapp
 echo tag - %tag%
 docker build -t bhanudocker3315/regapp:latest -t bhanudocker3315/regapp:%tag% %WORKSPACE%
-docker run -d --name simple-devops-container -p 8090:8080 bhanudocker3315/regapp
+docker run -d --name simple-regapp -p 8090:8080 bhanudocker3315/regapp
