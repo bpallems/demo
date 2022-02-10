0c3925cad9f6d05523749b625aee3308089e3ddf
Bhanu Pallemsetty <bhanu_pallemsetty@persistent.com>
2022-02-10 20:27:06 +0530

    updated file

--- tag_push_deploy.bat
+++ tag_push_deploy.bat
@@ -6,4 +6,4 @@ docker build -t bhanudocker3315/regapp:latest -t bhanudocker3315/regapp:%tag% %W
 docker push bhanudocker3315/regapp:latest
 docker push bhanudocker3315/regapp:%tag%
 REM docker run -d --name simple-regapp -p 8090:8080 bhanudocker3315/regapp
-kubectl apply -f k8s
+kubectl --kubeconfig="c:\users\bhanu_pallemsetty\.kube\config" apply -f k8s
