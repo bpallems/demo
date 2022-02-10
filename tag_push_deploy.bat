340be0a8db7b5005a64853bfc82f6161f8b185e1
Bhanu Pallemsetty <bhanu_pallemsetty@persistent.com>
2022-02-10 19:23:34 +0530

    updated files

---
+++ tag_push_deploy.bat
@@ -0,0 +1,9 @@
+COPY %WORKSPACE%\webapp\target\webapp.war %WORKSPACE%
+REM docker stop simple-regapp
+REM docker rm simple-regapp
+echo tag - %tag%
+docker build -t bhanudocker3315/regapp:latest -t bhanudocker3315/regapp:%tag% %WORKSPACE%
+docker push bhanudocker3315/regapp:latest
+docker push bhanudocker3315/regapp:%tag%
+REM docker run -d --name simple-regapp -p 8090:8080 bhanudocker3315/regapp
+kubectl apply -f %WORKSPACE%\k8s
